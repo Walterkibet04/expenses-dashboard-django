4223f8a2a51bbecde2a2b1e25743e59ac0404998
Walter kibet <kibetwalter726@gmail.com>
2024-06-30 12:48:06 +0300

    update

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.contrib.auth.base_user import BaseUserManager
 
 # Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email is a required field')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using = self. _db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+
+class CustomUser(AbstractUser, PermissionsMixin):
+    email = models.EmailField(max_length=200,unique=True)
+    username = models.CharField(max_length=200, null=True, blank=True)
+
+    user.is_active = models.BooleanField(default=True)
+    user.is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
